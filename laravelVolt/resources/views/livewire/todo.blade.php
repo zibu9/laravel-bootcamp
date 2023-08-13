a9bbb4e380aef5a9b77d37eb527923373a44a5ed
Zibu <58338897+zibu9@users.noreply.github.com>
2023-08-13 07:29:10 +0100

    implement todo component logic

--- laravelVolt/resources/views/livewire/todo.blade.php
+++ laravelVolt/resources/views/livewire/todo.blade.php
@@ -1,11 +1,44 @@
 <?php
 
-use function Livewire\Volt\{state};
+    use App\Models\Todo;
 
-//
+    use function Livewire\Volt\{state, rules, computed};
+
+    state([
+        'content' => '',
+    ]);
+
+    rules([
+        'content' => [
+            'required',
+            'min:3',
+            'max:120'
+        ],
+    ]);
+
+    $addTodo = function() {
+        $this->validate();
+        Todo::create(['content'=> $this->content]);
+        $this->content = '';
+    };
+
+    $count = computed(function() {
+        return Todo::count();
+    });
+
 
 ?>
 
 <div>
-    //
+    AllTask : {{ $this->count }}
+    <form wire:submit.prevent="addTodo">
+        <label for="content">
+            content
+        </label>
+        <input type="text" id="content" wire:model="content">
+        <button type="submit">Add Task</button>
+    </form>
+    @error('content')
+        <span>{{ $message }}</span>
+    @enderror
 </div>
